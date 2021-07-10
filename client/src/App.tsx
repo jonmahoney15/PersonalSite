3802a90066d995166bbada0aa4b91673413a53cb
jonmahoney15 <jonmahoney15@yahoo.com>
2021-07-09 23:56:35 -0400

    nav bar base

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import Header from './components/Header';
+import Navbar from './components/Navbar';
 import "./App.css";
 
 function App() {
   return (
     <div >
-      <Header/>
+      <Navbar />
+      <div>
+      </div> 
     </div>
   );
 }
---
+++ client/src/components/Navbar.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Navbar() {
+    return( 
+        <>
+            <ul>
+                <li>Home</li>
+                <li>News</li>
+                <li>Contact</li>
+                <li>About</li>
+            </ul>
+        </>
+    );
+}
+
+export default Navbar;
