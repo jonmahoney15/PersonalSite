5a54a36ed00d2b35988d120b2a6767705eb8e91d
Jonathan Mahoney <jonmahoney15@yahoo.com>
2021-07-14 23:57:10 -0400

    updated folder structure to work with svg

--- client/src/components/Card.tsx
+++ client/src/components/Card.tsx
@@ -9,13 +9,13 @@ interface CardProp {
 
 const Card = (props: CardProp) => {
   return (
-      <div id="Card">
-          <div className="align-middle p-10 bg-blue-900 rounded">
-            <props.icon style={{height: 100, width: 100}}/>
+      <div className="m-5" id="Card">
+          <div className="flex justify-center align-middle p-10 bg-blue-900 rounded">
+            <props.icon style={{height: 90, width: 90}}/>
           </div>
           <div className="description">
             <div>
-              <p className="text-black w-10 md:w-48 lg:w-98">{props.description}</p>
+              <p className="text-black w-32 md:w-48 lg:w-98">{props.description}</p>
             </div>
           </div>
       </div>
