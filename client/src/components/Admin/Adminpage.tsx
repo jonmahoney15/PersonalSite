a97574e5227477b970dc04aa73c8061fb4b74b7e
Jonathan Mahoney <jonmahoney15@yahoo.com>
2021-08-27 19:36:46 -0400

    updated db

--- client/src/components/Admin/Adminpage.tsx
+++ client/src/components/Admin/Adminpage.tsx
@@ -10,49 +10,54 @@ interface IPosts {
 
 const Adminpage = () => {
     const [posts, setPosts] = useState<IPosts[]>([]);
+    const [error, setError] = useState("");
 
     useEffect(() => {
         const getPosts = async () => {
             try {
                 let response = await axios.get('/api/blog/Posts')
                 let data = response.data;
-                console.log(data);
                 let newPosts: IPosts[] = data.map((e: IPosts) => e);
-                console.log(newPosts);
                 setPosts(newPosts);
             } catch(error) {
-               console.log(error); 
+               setError(error);
             }
         }
 
         getPosts();
     }, []);
 
+    const buttons = () => {
+        return (
+            <Modal Title="Delete Post" ButtonTitle="Delete" CloseButtonLabel="Yes" ChildComponent={<p>Are you sure you want to delete this blog?</p>}/>
+        );    
+    }
+
     const getRows = () => {
-        console.log(posts);
         let rows = posts.map(post => {
-            return(<tr>
-                <td>{post.Title}</td>
-                <td>{post.Date}</td>
-                <td>buttons</td>
+            return (
+            <tr>
+                <td className="px-8 py-4 text-left border border-gray-100">{post.Title}</td>
+                <td className="px-8 py-4 text-left border border-gray-100">{post.Date}</td>
+                <td className="px-8 py-4 text-left border border-gray-100">{buttons()}</td>
             </tr>);
             })
         return rows;
     }
 
     return (
-        <div className="flex flex-col items-center">
-            <h1>Admin Page</h1>
+        <div className="flex flex-col items-center bg-white bg-cover">
+            <h1 className="text-5xl">Admin Page</h1>
             <div>
-                <h3>Blog Posts</h3>
-                <table className="bg-white border-black border-solid rounded shadow-lg">
+                <h3 className="flex justify-center text-3xl m-5">Blog Posts</h3>
+                <table className="shadow-lg">
                     <tbody>
                         <tr>
                             <th className="px-8 py-4 text-left bg-blue-100 border-none rounded-tl-2xl">Title</th>
                             <th className="px-8 py-4 text-left bg-blue-100 border-none">Date</th>
                             <th className="px-8 py-4 text-left bg-blue-100 border-none rounded-tr-2xl">Actions</th>
                         </tr>
-                        {getRows()}
+                        {posts && posts.length > 0 ? getRows() : <p>{error}</p>}
                     </tbody>
                 </table>
             </div>
